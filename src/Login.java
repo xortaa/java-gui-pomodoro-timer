27ba08c0f76fcdbfeb6af6a27a997ac568449bf5
xortaa <kercwinpaul@gmail.com>
2022-05-29 03:46:29 +0800

    login layout

--- src/Login.java
+++ src/Login.java
@@ -2,8 +2,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.Color;
 import javax.swing.JPanel;
-import javax.swing.BorderFactory;
-import javax.swing.border.Border;
 
 public class Login extends JFrame{
 
@@ -12,15 +10,15 @@ public class Login extends JFrame{
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(438, 720);
         this.setResizable(false);
-        this.setLayout(new GridBagLayout());
-        this.getContentPane().setBackground(Color.BLACK);
+        this.setLayout(new GridLayout(1,1));
+        this.setTitle("O CLOCK");
 
         // logo wrapper
         JPanel logoWrapper = new JPanel();
         ImageIcon image = new ImageIcon("assets/logo.png");
         JLabel label = new JLabel();
         label.setIcon(image);
-        logoWrapper.setBackground(Color.BLACK);
+        logoWrapper.setBackground(Color.decode("#171515"));
         this.setIconImage(image.getImage());
         logoWrapper.add(label);
 
@@ -29,53 +27,80 @@ public class Login extends JFrame{
         GridBagConstraints gc = new GridBagConstraints();
         //username////////////////////
         JLabel usernameLabel = new JLabel("USERNAME");
-        usernameLabel.setFont(new Font("Arial", Font.PLAIN, 20));
-        usernameLabel.setBackground(Color.RED);
+        usernameLabel.setForeground(Color.WHITE);
+        usernameLabel.setFont(new Font("Arial", Font.PLAIN, 15));
         JTextField usernameTextField = new JTextField();
-        usernameTextField.setText("username");
+        usernameTextField.setBackground(Color.decode("#113b54"));
+        usernameTextField.setForeground(Color.WHITE);
+        
+        usernameTextField.setPreferredSize(new Dimension(338, 30));
         //password//////////////////
         JLabel passwordLabel = new JLabel("PASSWORD");
-        passwordLabel.setFont(new Font("Arial", Font.PLAIN, 20));
-        JTextField passwordTextField = new JTextField();
-        //loginbun////////////////////////
+        passwordLabel.setForeground(Color.WHITE);
+        passwordLabel.setFont(new Font("Arial", Font.PLAIN, 15));
+        JPasswordField passwordField = new JPasswordField();
+        passwordField.setBackground(Color.decode("#113b54"));
+        passwordField.setForeground(Color.WHITE);
+        passwordField.setPreferredSize(new Dimension(338, 30));
+        //loginbtn////////////////////////
         JButton loginBtn = new JButton("LOGIN");
-        loginBtn.setPreferredSize(new Dimension(30, 30));
-        formWrapper.add(usernameLabel);
-        formWrapper.add(usernameTextField);
-        formWrapper.add(passwordLabel);
-        formWrapper.add(passwordTextField);
-        formWrapper.add(loginBtn);
-        formWrapper.setBackground(Color.BLACK);
+        loginBtn.setFocusable(false);
+        loginBtn.setBackground(Color.decode("#545454"));
+        loginBtn.setForeground(Color.WHITE);
+        loginBtn.setPreferredSize(new Dimension(100, 40));
+
+        formWrapper.setBackground(Color.decode("#171515"));
         
-        gc.weightx = 0.5;
-        gc.weighty = 0.5;
-        gc.anchor = GridBagConstraints.LINE_START;
+        gc.anchor = GridBagConstraints.FIRST_LINE_START;
         gc.gridx = 0; 
         gc.gridy = 0;
+        gc.weighty = 1;
         formWrapper.add(usernameLabel, gc);
 
+        gc.anchor = GridBagConstraints.FIRST_LINE_START;
+        gc.weighty = 1;
         gc.gridx = 0;
         gc.gridy = 1; 
         formWrapper.add(usernameTextField, gc);
 
+        gc.anchor = GridBagConstraints.FIRST_LINE_START;
         gc.gridx = 0; 
         gc.gridy = 2; 
         formWrapper.add(passwordLabel, gc);
         
+        gc.anchor = GridBagConstraints.FIRST_LINE_START;
         gc.gridx = 0;
         gc.gridy = 3; 
-        formWrapper.add(passwordTextField, gc);
+        formWrapper.add(passwordField, gc);
 
+        gc.weighty = 10;
+        gc.anchor = GridBagConstraints.CENTER;
         gc.gridx = 0;
         gc.gridy = 4; 
         formWrapper.add(loginBtn, gc);
 
 
 
-        JPanel container = new JPanel(new GridLayout(2,1,0 ,20));
-        container.setBackground(Color.BLACK);
-        container.add(logoWrapper);
-        container.add(formWrapper);
+        JPanel container = new JPanel(new GridBagLayout());
+        container.setBackground(Color.decode("#171515"));
+
+        GridBagConstraints c = new GridBagConstraints();
+
+        c.weighty = 0.4;
+        c.gridx = 0;
+        c.gridy = 0; 
+        container.add(logoWrapper, c);
+
+        // c.weightx = 0.5; 
+        c.anchor = GridBagConstraints.FIRST_LINE_START;
+        c.fill = GridBagConstraints.VERTICAL;
+        c.weighty = 0.6;
+        c.gridx = 0; 
+        c.gridy = 1;
+        container.add(formWrapper, c);
+
+
+
         
         
         
