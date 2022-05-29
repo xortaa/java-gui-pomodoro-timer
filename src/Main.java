70141000aad8d4e51c8af1b8ff711e3ebda3e00e
xortaa <kercwinpaul@gmail.com>
2022-05-29 15:10:35 +0800

    called johns in the main class

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,6 @@
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.text.DecimalFormat;
-
-import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) throws Exception{
-        
+      new JohnS();  
     }
 }
